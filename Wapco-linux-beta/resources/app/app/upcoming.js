dc9c0edfc56fd7172203ad3779eb035b28abf2f4
PranjalMishra11 <pranjal2k16@gmail.com>
2018-10-10 21:49:39 +0530

    wapco

--- Wapco-linux-beta/resources/app/app/upcoming.js
+++ Wapco-linux-beta/resources/app/app/upcoming.js
@@ -12,13 +12,21 @@ function show_up() {
 	let pyPath = window.controller.getPythonPath()
   	let pyDir = window.controller.getPythonAppDir()
   	let script = path.join(pyDir, 'upcoming.py')
-	var command = pyPath.pythonBin + " " + script + " show"
+	var command = pyPath.pythonBin + " " + script
 	exec(command, function (error, stdout, stderr) {
 		if (error !== null) {
 	        console.log('exec error: ' + error)
 	        document.write(error);
 	        return;
 	    }
+	    var d = 0;
+	    for(var i = 0; i < stdout.length; i ++) {
+	    	if(stdout[i] == '<') {
+	    		d = i;
+	    		break;
+	    	}
+	    }
+	    stdout = stdout.substring(d, stdout.length);
 	    document.write(stdout);
 	});
 }
@@ -27,13 +35,21 @@ function upd_up() {
 	var exec = require('child_process').exec; 
 	let pyPath = window.controller.getPythonPath()
   	let pyDir = window.controller.getPythonAppDir()
-  	let script = path.join(pyDir, 'upcoming.py')
-	var command = pyPath.pythonBin + " " + script + " upd"
+  	let script = path.join(pyDir, 'update.py')
+	var command = pyPath.pythonBin + " " + script
 	exec(command, function (error, stdout, stderr) {
 		if (error !== null) {
 	        console.log('exec error: ' + error);
 	        return;
 	    }
+	    var d = 0;
+	    for(var i = 0; i < stdout.length; i ++) {
+	    	if(stdout[i] == '<') {
+	    		d = i;
+	    		break;
+	    	}
+	    }
+	    stdout = stdout.substring(d, stdout.length);
 	    document.write(stdout);
 	});
 }
